d7ad46a94f358bada7beb5bc9731fe46e72f1be3
Imacc2009 <josephfra@btinternet.com>
2014-03-01 11:59:13 +0000

    Pull-request
    Pull-request branch.

---
+++ gamemode/schema/items/clothing/sh_uniform.lua
@@ -0,0 +1,7 @@
+ITEM.name = "Citizen Outfit"
+ITEM.desc = "A blue shirt to cover up the beige shirt."
+ITEM.model = Model("models/humans/group01/male_04.mdl")
+ITEM.replacement = {"group(%d+)", "group01"}
+ITEM.price = 0
+ITEM.flag = "v"
+ITEM.weight = -2.5
